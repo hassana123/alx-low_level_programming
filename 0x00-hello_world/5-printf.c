26684406469a254d83b9700fade57e91fbae979a
hassana123 <hassanaabdll1@gmail.com>
2023-03-16 16:54:18 -0700

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 /**
-* main - Entry point 
-* 
-* Return Always 0 (Succes)
-*/
+ * main - A program that display  a sentence using the printf function
+ *
+ * Return Always 0 (Succes)
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*
-* Return Always 0 (Succes)
-*/
+ * main - A program that prints the size of various types on the computer it is compiled and run on.
+ *
+ * Return: 0 (succes)
+ */
 int main(void)
 {
-	printf("Size of a char: %d bytes(s)\n", sizeof(char));
-	printf("Size of a int: %d bytes(s)\n", sizeof(int));
-	printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
-       	printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
-       	printf("Size of a float: %d bytes(s)\n", sizeof(float));
+printf("Size of a char: %d bytes(s)\n", sizeof(char));
+printf("Size of a int: %d bytes(s)\n", sizeof(int));
+printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
+printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
+printf("Size of a float: %d bytes(s)\n", sizeof(float));
+return (0);
 }
