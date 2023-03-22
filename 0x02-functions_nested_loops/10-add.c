cb88f5a78d3ce5c96618a9823635b6a9061182f2
hassana123 <hassanaabdll1@gmail.com>
2023-03-21 17:07:22 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add -  a program that takes to integers and adds them up
+ * @n: first integer to be taken
+ * @m: second integer to be taken
+ * Return: sum of the integers
+ */
+int add(int n, int m)
+{
+	int sum;
+
+	sum = n + m;
+	return (sum);
+}
