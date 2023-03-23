9975a7819ef6358f3a8ea7d343e27cc2e0f3b962
hassana123 <hassanaabdll1@gmail.com>
2023-03-23 14:44:23 -0700

    alldone

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - a program that causes an infinte loop
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incomming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
+
+	printf("infinite loop avoided! \\o/\n");
+
+	return (0);
+}
